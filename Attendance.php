aaf545f319beb3841dea07a78dfe7ee0210ac07d
wirahmat <58240412+wirahmat@users.noreply.github.com>
2021-12-15 18:02:41 +0700

    attendance

---
+++ Attendance.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Sanchez&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <link href="./css/attendance.css" rel="stylesheet" /><title>Document</title>
+    </head>
+    <body>
+        <div class="v57_531">
+            <span class="v57_532">Attendance</span>
+            <a href="" style="text-decoration:none; color:black;">
+                <div class='kotak'>
+                    <span class="bigtext"><center>Session 1 January 3</center></span><br>
+                    <span class="text">ID : A00000002</span>
+                </div>
+            </a> 
+        </div>
+    </body>
+</html>
