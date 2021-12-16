ee85b7799aa622e5d565747f7fb43edc49cb7f8d
wirahmat <58240412+wirahmat@users.noreply.github.com>
2021-12-16 10:10:16 +0700

    attendance view fix
    tapi button belom jalan sama id

--- attendance_view.php
+++ attendance_view.php
@@ -6,9 +6,6 @@ session_start();
 if(!isset($_SESSION["user"])) header("Location: index.php");
 
 $stmt = $db->query("SELECT * from studentattendance where attendance_id = 1");
-$row = $stmt->fetch();
-$id=$row['attendance_id'];
-$lengthid=strlen((string)$id); 
 ?>
 
 <html>
@@ -22,7 +19,7 @@ $lengthid=strlen((string)$id);
     <body>
         <div class="v57_388">
             <span class="v57_389">Attendance</span>
-            <span class="v86_122">ID : AT<?php while ($lengthid < 8){echo "0"; $lengthid=$lengthid+1;}echo $id ?></span>
+            <span class="v86_122">ID : AT</span>
             <div class="v57_472">
                 <span class="v57_473">Session 1</span>
                 <span class="v57_474">January 3</span>
@@ -36,10 +33,12 @@ $lengthid=strlen((string)$id);
                     $sid=$row['student_id'];
                       
 				echo "<div class='kumpul'>
-                <span class='v57_392'>".$name."</span>
-                <button type='submit' class='v57_424'>UNATTEND</button>
-                <button type='submit' class='v57_448'>PERMIT</button>
-                <button type='submit' class='v57_400'>ATTEND</button>
+                <span class='name'>".$name."</span>
+                <div class='button'>
+                    <button type='submit' class='v57_424' style='background: rgba(234,89,93,1);'>UNATTEND</button>
+                    <button type='submit' class='v57_424' style='background: rgba(189,229,26,1);'>PERMIT</button>
+                    <button type='submit' class='v57_424' style='background: rgba(95,221,136,1);'>ATTEND</button>
+                </div>
             </div>"	;
             } 
 				?> 
@@ -51,76 +50,85 @@ $lengthid=strlen((string)$id);
         </div>
     </body>
     <style>
+.button{
+            width: 325px;
+            height: 30px;
+            margin-top:5px;
+        }
         .kumpul{
             float:left;
-            width: 250px;
+            width: 325px;
             height: 70px;
             position: relative;
             margin-top: 5px;
-            top: -5px;
-            left: 0px;
+            top: 100px;
+            left: 70px;
+            font-size: 16px;
+        }
+        .name{
+            margin-left: 15px;
+            font-family: Poppins;
+            font-weight: bold;
         }
     .v57_424 {
         width: 94px;
         height: 27px;
-        background: rgba(234,89,93,1);
         color: white;
+        margin-left: 10px;
         opacity: 1;
-        position: absolute;
-        overflow: hidden;
         border-top-left-radius: 16px;
         border-top-right-radius: 16px;
         border-bottom-left-radius: 16px;
         border-bottom-right-radius: 16px;
         }
-    .v57_448 {
-        width: 94px;
-        height: 27px;
-        background: rgba(189,229,26,1);
-        color: white;
-        opacity: 1;
-        overflow: hidden;
-        border-top-left-radius: 16px;
-        border-top-right-radius: 16px;
-        border-bottom-left-radius: 16px;
-        border-bottom-right-radius: 16px;
-    }
-    .v57_400 {
-        width: 94px;
-        height: 27px;
-        background: rgba(95,221,136,1);
-        color: white;
-        opacity: 1;
-        position: absolute;
-        overflow: hidden;
-        border-top-left-radius: 16px;
-        border-top-right-radius: 16px;
-        border-bottom-left-radius: 16px;
-        border-bottom-right-radius: 16px;
-    }
-        .v57_591 {
-  width: 73px;
-  height: 73px;
-  background: url("../images/v62_149.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  position: absolute;
-  top: 200px;
-  left: 320px;
-  overflow: hidden;
-}
 .v57_592 {
   width: 73px;
   height: 73px;
-  background: rgba(0, 52, 81, 1);
   position: fixed;
   top: 610px;
   left: 870px;
   border-radius: 50%;
   text-align: center;
-  color: rgba(255, 255, 255, 1);
-  font-size: 100px;
+  color: rgba(0, 52, 81, 1);
+  font-size: 90px;
+  font-weight: bold;
+}
+        .v57_472 {
+  width: 100px;
+  height: 52px;
+  background: url("../images/v57_472.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 14px;
+  left: 276px;
+  overflow: hidden;
+}
+.v57_473 {
+  width: 88px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 0px;
+  left: 12px;
+  font-family: Poppins;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: right;
+}
+.v57_474 {
+  width: 100px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 22px;
+  left: 0px;
+  font-family: Poppins;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: right;
 }
     </style>
 </html>
